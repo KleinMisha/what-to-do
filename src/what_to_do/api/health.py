c6c518e30060571f142abd8e826afa5c29ca6bb8
KleinMisha <klein.misha@gmail.com>
2026-09-05 00:02:01 +0200

    Add response status code.

--- src/what_to_do/api/health.py
+++ src/what_to_do/api/health.py
@@ -1,10 +1,10 @@
 """Health check (standard endpoint)."""
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
-router = APIRouter()
+router = APIRouter(tags=["Health"])
 
 
-@router.get("/health")
+@router.get("/health", status_code=status.HTTP_200_OK)
 def health():
     return {"status": "ok"}
