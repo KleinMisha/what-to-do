ba5b23f31eddda827f5d53f70979b44bc1e98202
KleinMisha <klein.misha@gmail.com>
2026-09-12 17:52:37 +0200

    CLI entrypoint: registered command groups.

--- src/what_to_do/cli/entrypoint.py
+++ src/what_to_do/cli/entrypoint.py
@@ -1,6 +1,15 @@
 from typer import Typer, echo
 
+from what_to_do.cli.routing.groups import app as groups_app
+from what_to_do.cli.routing.projects import app as projects_app
+from what_to_do.cli.routing.tasks import app as tasks_app
+
 app = Typer(no_args_is_help=True)
+app.add_typer(
+    tasks_app, name="tasks", help="Manage tasks - Primary items (lowest level)"
+)
+app.add_typer(projects_app, name="projects", help="Manage projects")
+app.add_typer(groups_app, name="groups", help="Manage task/project groups")
 
 
 @app.command()
