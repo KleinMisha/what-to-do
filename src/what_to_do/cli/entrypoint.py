80491354ccf01ff1a1ebdf1ad3063227c6aa7871
KleinMisha <klein.misha@gmail.com>
2026-09-13 02:14:38 +0200

    minor change

--- src/what_to_do/cli/entrypoint.py
+++ src/what_to_do/cli/entrypoint.py
@@ -5,9 +5,7 @@ from what_to_do.cli.routing.projects import app as projects_app
 from what_to_do.cli.routing.tasks import app as tasks_app
 
 app = Typer(no_args_is_help=True)
-app.add_typer(
-    tasks_app, name="tasks", help="Manage tasks - Primary items (lowest level)"
-)
+app.add_typer(tasks_app, name="tasks", help="Manage tasks")
 app.add_typer(projects_app, name="projects", help="Manage projects")
 app.add_typer(groups_app, name="groups", help="Manage task/project groups")
 
